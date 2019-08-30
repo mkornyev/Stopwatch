c3991c9b85fa56bf62ec319f91528fd22b4e8ab0
mkornyev-cmu <mkornyev@andrew.cmu.edu>
2019-08-30 11:04:33 -0400

    Stage commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Max Kornyev on 8/30/19.
+//  Copyright © 2019 Max Kornyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
